6989c29813560b26dae101f5732b26bbe6ca568f
tinaboebina <147528437+tinaboebina@users.noreply.github.com>
2024-03-07 22:33:51 -0700

    added comments

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-var acc = document.getElementsByClassName("accordion");
+//----- TOGGLE ACCORDIAN -----//
+var acc = document.getElementsByClassName("accordion"); // SELECT ACCORDIAN
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
@@ -17,14 +15,12 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-// Function to update the footer date
+//----- UPDATE FOOTER TO CURRENT YEAR -----//
 function updateFooterDate() {
     var currentYear = new Date().getFullYear();
     document.getElementById("currentYear").textContent = currentYear;
   }
 
-  // Call the function initially to set the initial footer date
   updateFooterDate();
 
-  // Set an interval to update the footer date every year
-  setInterval(updateFooterDate, 1000 * 60 * 60 * 24 * 365); // Update every year
+  setInterval(updateFooterDate, 1000 * 60 * 60 * 24 * 365);
